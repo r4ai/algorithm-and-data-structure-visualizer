4979f10372e7a82de386cbb059cbc452314383f5
rai <96561881+r4ai@users.noreply.github.com>
2025-02-19 01:11:08 +0900

    chore: bun -> pnpm

--- eslint.config.js
+++ eslint.config.js
@@ -8,6 +8,8 @@ export default antfu(
       indent: 2,
       quotes: "double",
     },
+    typescript: true,
+    ignores: ["node_modules/**", "dist/**", "src/routeTree.gen.ts"],
   },
   ...tailwind.configs["flat/recommended"],
 )
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,5 @@
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router"
 import { TanStackRouterDevtools } from "@tanstack/router-devtools"
-import * as React from "react"
 
 export const Route = createRootRoute({
   component: RootComponent,
